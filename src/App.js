9a58fe70d873fb4e674fee1d2e8182caa1fe2ac6
Margo Kelley <margo.kelley13@gmail.com>
2022-03-12 23:01:10 -0500

    document app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-// TODO: import useFormik from formik library
+// import useFormik from formik library
 import { useFormik } from "formik";
 
 function App() {
-  // TODO: add a const called formik assigned to useFormik()
+  // add a const called formik assigned to useFormik()
   const formik = useFormik({
+    // initial form
     initialValues: {
       email: '',
       password: '',
     },
+    // add alert, if validated
     onSubmit: values =>{
       alert("Login Successful");
     },
+    // add validation to form
     validate: values =>{
       let errors = {};
       if(!values.email) {
@@ -24,6 +27,7 @@ function App() {
     }
   });
 
+  // handle form info + give errors if not validated
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
